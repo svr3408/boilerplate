20730a2a20a7e1a9b23e337b252c75f594bff79a
vitaliy <svr3408@gmail.com>
2019-07-23 11:37:28 +0500

    fix: gitignore

--- dest/main.js
+++
@@ -1,2 +0,0 @@
-"use strict";class User{constructor(e){this.name=e}sayHi(){console.log(this.name)}}document.addEventListener("DOMContentLoaded",()=>{new User("Вася").sayHi()});
-//# sourceMappingURL=main.js.map
